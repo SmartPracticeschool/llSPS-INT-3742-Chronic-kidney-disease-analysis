aeaad8fa99247c6458c669294aefbe646f40a179
varshini999 <70567070+varshini999@users.noreply.github.com>
2020-09-07 00:00:06 +0530

    Add files via upload

---
+++ ckd/app.py
@@ -0,0 +1,27 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('ckd.h5', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/y_predict',methods=['POST'])
+def y_predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    x_test = [[int(x) for x in request.form.values()]]
+
+    prediction = model.predict(x_test)
+    print(prediction)
+    output=prediction[0][0]
+    return render_template('index.html',
+  prediction_text=
+  'Prone to Chronic Kidney Disease: {}'.format(output))
+  
+if __name__ == "__main__":
+    app.run(debug=True)
